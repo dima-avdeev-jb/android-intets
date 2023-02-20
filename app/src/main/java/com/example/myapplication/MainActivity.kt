db5132418aa1e431d8940bd9aa1a5ae1d3053687
dima.avdeev <dima.avdeev@jetbrains.com>
2023-02-20 17:14:49 +0400

    imePadding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,12 +3,20 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowCompat
 import com.example.myapplication.ui.theme.MyApplicationTheme
@@ -24,25 +32,47 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    Content()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        Content()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Content() {
+    val t = remember { mutableStateOf("text state") }
+    Box(
+        Modifier
+            .fillMaxSize()
+            .navigationBarsPadding()
+            .imePadding()//keyboard
+    ) {
+        Text(
+            buildString { repeat(10) { appendLine("Up ${it + 1}") } },
+            Modifier
+                .align(Alignment.TopStart)
+                .background(Color.LightGray)
+        )
+        TextField(t.value, { t.value = it },
+            Modifier
+                .align(Alignment.Center)
+                .fillMaxWidth())
+        Text(
+            buildString { repeat(10) { appendLine("Down ${it + 1}") } },
+            Modifier
+                .align(Alignment.BottomStart)
+                .background(Color.LightGray)
+        )
     }
 }
