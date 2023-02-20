b28617c1c76a590b64b8bafd70f77a18938497da
dima.avdeev <dima.avdeev@jetbrains.com>
2023-02-20 22:26:04 +0400

    displayCutoutPadding

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,64 +5,28 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.*
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
-import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
-            MyApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Scaffold(topBar = {
-                        NavigationBar(containerColor = Color.Transparent) {
-                            Box(Modifier.fillMaxSize()) {
-                                Box(
-                                    Modifier
-                                        .fillMaxHeight()
-                                        .align(Alignment.CenterEnd)
-                                        .background(Color.LightGray)
-                                ) {
-                                    Text("NavigationBar",
-                                        Modifier
-                                            .align(Alignment.CenterEnd)
-                                            .padding(start = 10.dp, end = 100.dp)
-                                    )
-                                }
-                            }
-                        }
-                    }) {
-                        Content()
-                    }
-                }
-            }
+            Content()
         }
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Content() {
     val t = remember { mutableStateOf("text state") }
@@ -70,12 +34,12 @@ fun Content() {
         Modifier
             .background(Color.Yellow)
             .displayCutoutPadding()
-            .background(Color.Green)
-            .statusBarsPadding()
+//            .background(Color.Green)
+//            .statusBarsPadding()
             .background(Color.Blue)
             .navigationBarsPadding()//bottom
             .imePadding()//keyboard
-            .background(Color.Red)
+            .background(Color.LightGray)
             .fillMaxSize()
     ) {
         Text(
@@ -84,7 +48,7 @@ fun Content() {
                 .align(Alignment.TopStart)
                 .background(Color.LightGray)
         )
-        TextField(
+        BasicTextField(
             t.value, { t.value = it },
             Modifier
                 .align(Alignment.Center)
@@ -99,10 +63,3 @@ fun Content() {
     }
 }
 
-@Preview(showBackground = false, device = "id:pixel_5", showSystemUi = true)
-@Composable
-fun GreetingPreview() {
-    MyApplicationTheme {
-        Content()
-    }
-}
